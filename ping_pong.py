7b3c5b0e4473737609f7aca749a0cfd8c8d64f24
Fellipe Castro <contact@fellipecastro.com>
2015-06-14 13:22:36 -0300

    Update

--- ping_pong.py
+++ ping_pong.py
@@ -4,30 +4,30 @@
 
 def ping_pong(number):
     # 3 and 5
-    if check_number_is_visible_by_15(number):
+    if __check_number_is_visible_by_15(number):
         return 'ping-pong'
-    if check_number_is_visible_by_3(number):
+    if __check_number_is_visible_by_3(number):
         return 'ping'
-    if check_number_is_visible_by_5(number):
+    if __check_number_is_visible_by_5(number):
         return 'pong'
     else:
         return number
 
 
-def check_number_is_visible_by_15(number):
+def __check_number_is_visible_by_15(number):
     return number % 15 == 0
 
 
-def check_number_is_visible_by_3(number):
+def __check_number_is_visible_by_3(number):
     return number % 3 == 0
 
 
-def check_number_is_visible_by_5(number):
+def __check_number_is_visible_by_5(number):
     return number % 5 == 0
 
 
 def main():
-    for i in range(1, 101):
+    for i in xrange(1, 101):
         print 'ping_pong(%i) = %s' % (i, ping_pong(i))
 
 if __name__ == '__main__':
